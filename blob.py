162ca10b043519b6985b6addaa4fe282cfcf18d9
Lynn 'Cyrin' Conway <lynncyrin@gmail.com>
2014-08-27 00:22:57 -0700

    changes from test results

--- blob.py
+++ blob.py
@@ -25,45 +25,42 @@ class blob (object):
         #more!
     ]
 
-    def __init__ (self, blob):
+    def __init__ (self):
         R = 0; G = 1; B = 2
-        self.blob = blob
-        #init colors
-        color = blob.color_list(random.randrange(0,len(blob.color_list)))
+
+        color = blob.color_list[random.randrange(0,len(blob.color_list))]
         self.color = [color[R], color[G], color[B]]
         self.color_name = color[3]
-        #/colors
-        #init attraction
-        R_ideal = ideal_R(self.blob(colors(R)))
-        G_ideal = ideal_R(self.blob(colors(G)))
-        B_ideal = ideal_R(self.blob(colors(R,G,B)))
-        self.ideals = [R_ideal, G_ideal, B_ideal]
-        #/attraction
 
-    def ideal_R (self, R):
+        self.ideals = [self.ideal_R(), self.ideal_G(), self.ideal_B()]
+
+    def ideal_R (self):
         '''
         red is het, attraction to colors that are different from you
         the less red you are, the more you are attracted to red
         sky blues (0,255,255) are most attracted to reds (255,0,0)
         '''
-        return (-R+255)
+        return (-self.color[0]+255)
 
-    def ideal_G (self, G):
+    def ideal_G (self):
         '''
         green is gay, attraction to colors like yourself
         the more green you are, the more you are attracted to green
         greens (0,255,0) are most attracted to greens (0,255,0)
         '''
-        return (G)
+        return (self.color[1])
 
-    def ideal_B (self, R, G, B):
+    def ideal_B (self):
         '''
         blue is pansexuality, attraction to all colors
         the more color you have, the more you are attracted to blue
         blacks (255,255,255) are most attracted to blues (0,0,255)
         '''
-        return ((R+G+B)/(3))
+        return ((self.color[0]+self.color[1]+self.color[2])/(3))
 
     #WIP
     def attraction_to (self, other):
         R = 0; G = 1; B = 2
+
+if __name__ == '__main__':
+    b = blob()
