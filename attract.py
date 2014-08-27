4ec0671ac99a7bc68852c623a876e544d4a477e3
Lynn 'Cyrin' Conway <lynncyrin@gmail.com>
2014-08-26 23:41:52 -0700

    changed attraction to 'ideals'
    attraction will be derived from ideals later

--- attract.py
+++ attract.py
@@ -8,28 +8,35 @@ class attract (object):
     def __init__ (self, blob):
         R = 0; G = 1; B = 2
         self.blob = blob
-        R_attract = attract_to_R(self.blob(colors(R)))
-        G_attract = attract_to_R(self.blob(colors(R)))
-        B_attract = attract_to_R(self.blob(colors(R)))
-        self.attract(R_attract, G_attract, B_attract)
+        R_ideal = ideal_R(self.blob(colors(R)))
+        G_ideal = ideal_R(self.blob(colors(G)))
+        B_ideal = ideal_R(self.blob(colors(R,G,B)))
+        self.ideals(R_ideal, G_ideal, B_ideal)
 
-    def attract_to_R (self, R):
+    def ideal_R (self, R):
         '''
-        Red is het, and thusly not very attracted to other reds
-        Red's ideal partner is 0,255,255 == sky blue
+        Red is het, attraction to colors that are different from you
+        The less red you are, the more you are attracted to red
+        sky blues (0,255,255) are most attracted to reds (255,0,0)
         '''
         return (-R+255)
 
-    def attract_to_G (self, G):
+    def ideal_G (self, G):
         '''
-        Green is gay, and thusly attracted to other greens
-        Green's ideal partner is 0,255,0 == green
+        Gay is gay, attraction to colors like yourself
+        The more green you are, the more you are attracted to green
+        greens (0,255,0) are most attracted to greens (0,255,0)
         '''
         return (G)
 
-    def attract_to_B (self, R, G, B): return 133
+    def ideal_B (self, R, G, B):
         '''
-        Blue is pan, and is more attracted to you the more color you have
-        Blue's ideal partner is 255,255,255 == black
+        Blue is pansexuality, attraction to all colors
+        The more color you have, the more you are attracted to blue
+        blacks (255,255,255) are most attracted to blues (0,0,255)
         '''
         return ((R+G+B)/(3))
+
+    #WIP
+    def attraction (self, other):
+        R = 0; G = 1; B = 2
